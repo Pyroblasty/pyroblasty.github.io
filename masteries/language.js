fe63b00bf2fdcdaefd9715ad82ae993f348e6423
Pyroblasty <lukeatlook@gmail.com>
2015-11-03 00:27:31 +0100

    update

--- masteries/language.js
+++ masteries/language.js
@@ -1,10 +1,12 @@
 // In order to translate the calculator to another language, also change data.js and index.html
 // This file contains strings used for display purposes in calculator.js
+// Enable it by adding language.js to index.html (just uncomment the line)
 
-var languagePack_English = {
+var languagePack = {
     tree_names: ["Ferocity", "Cunning", "Resolve"],
     rank: "Rank",
     requires: "Requires",
     points_in: "points in",
     swap_warning: "Choosing this will remove points in",
+    doubleclick_to_reset: "Double click to reset tree",
 };
